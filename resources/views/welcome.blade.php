f07ff7fa30f45522b2d82449d635a5530f7913cb
domenicolafroscia <domenicolafroscia@gmail.com>
2024-01-15 17:51:58 +0100

    add: complete card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
-@section('content') 
-    <h1>contenuto</h1> 
+@section('content')
+    <div class="container">
+        <h1 class="my-3 text-center">Movies</h1>
+        <div class="row row-cols-5">
+            
+            @foreach ($movies as $movie)
+                <div class="col my-3">
+                    <div class="card w-100 h-100" style="width: 18rem;">
+                        <img src="{{ $movie->image }}" class="card-img-top" alt="poster">
+                        <div class="card-body">
+                            <h3 class="card-title">{{ $movie->title }}</h3>
+                            <h5>Original title: {{ $movie->original_title }}</h5>
+                            <p>Language: {{ $movie->nationality }}</p>
+                            <p>Setting date: {{ $movie->date }}</p>
+                            <p>Rating: {{ $movie->vote }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
 @endsection
